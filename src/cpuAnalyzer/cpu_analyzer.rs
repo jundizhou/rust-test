3057c0d667d5eac46d4b76b5f6b2c7dadd1f2890
jundizhou <jundizhou@harmonycloud.cn>
2023-06-07 17:27:25 +0800

    update code

--- src/cpuAnalyzer/cpu_analyzer.rs
+++ src/cpuAnalyzer/cpu_analyzer.rs
@@ -5,9 +5,9 @@ use std::rc::Rc;
 use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::{Duration, Instant};
+use byteorder::{ByteOrder, LittleEndian, ReadBytesExt};
 use crate::cpuAnalyzer::cpu_event::{CpuEvent, TimeSegments};
 use crate::probeToRust::KindlingEventForGo;
-use byteorder::{ByteOrder, LittleEndian, ReadBytesExt};
 use crate::cpuAnalyzer::circle_queue::CircleQueue;
 use crate::cpuAnalyzer::time_event::TimedEvent;
 use crate::cpuAnalyzer::cpu_event::Segment;
@@ -15,15 +15,13 @@ use crate::cpuAnalyzer::cpu_event::Segment;
 const NANO_TO_SECONDS: u64 = 1_000_000_000;
 const MAX_SEGMENT_SIZE: usize = 40;
 
-
-pub struct CpuAnalyzer  {
+pub struct CpuAnalyzer {
     pub cpu_pid_events: HashMap<u32, HashMap<u32, TimeSegments>>,
-    pub lock: Mutex<()>, // Assuming you have a mutex for synchronization
-    // cfg: Config,     // Assuming you have a Config struct defined
+    pub lock: Mutex<()>,
 }
 
-pub fn print_all_event(cca: &Arc<Mutex<CpuAnalyzer>>){
-    let mut ca_guard = cca.lock().unwrap();
+pub fn print_all_event(cca: &Arc<Mutex<CpuAnalyzer>>) {
+    let ca_guard = cca.lock().unwrap();
     ca_guard.print_cpu_pid_events();
 }
 
@@ -31,57 +29,35 @@ pub fn consume_cpu_event(event: &KindlingEventForGo, cca: &Arc<Mutex<CpuAnalyzer
     let mut ev = CpuEvent::default();
     for i in 0..event.paramsNumber as usize {
         let user_attributes = event.userAttributes[i];
-        //println!("{:?}", user_attributes.get_key());
         match user_attributes.get_key() {
             Some("start_time") => ev.start_time = user_attributes.get_uint_value(),
             Some("end_time") => ev.end_time = user_attributes.get_uint_value(),
-            Some("time_specs") =>{
-                let val = user_attributes.get_value().unwrap(); // Assuming val is always present
-                let mut cursor = Cursor::new(&val);
-                let type_specs_len = val.len() / 8;
-                let mut type_specs = vec![0u64; type_specs_len];
-
-                for i in 0..type_specs_len {
-                    type_specs[i] = cursor.read_u64::<LittleEndian>().unwrap();
-                }
-
-                ev.type_specs = type_specs;
+            Some("time_specs") => {
+                let val = user_attributes.get_value().unwrap();
+                ev.type_specs = read_u64_values(val);
             }
             Some("runq_latency") => {
-                let val = user_attributes.get_value().unwrap(); // Assuming val is always present
-                let mut cursor = Cursor::new(&val);
-                let runq_latency_len = val.len() / 8;
-                let mut runq_latency = vec![0u64; runq_latency_len];
-
-                for i in 0..runq_latency_len {
-                    runq_latency[i] = cursor.read_u64::<LittleEndian>().unwrap();
-                }
-
-                ev.runq_latency = runq_latency;
-            },
+                let val = user_attributes.get_value().unwrap();
+                ev.runq_latency = read_u64_values(val);
+            }
             Some("time_type") => {
-                let val = user_attributes.get_value().unwrap(); // Assuming val is always present
-                let mut cursor = Cursor::new(&val);
-                let time_type_len = val.len();
-                let mut time_type = Vec::with_capacity(time_type_len);
-                for _ in 0..time_type_len {
-                    let byte = cursor.read_u8().unwrap();
-                    time_type.push(byte);
-                }
-
-                ev.time_type = time_type;
-            },
-            Some("on_info") => ev.on_info = String::from_utf8_lossy(user_attributes.get_value().unwrap_or_default()).to_string(),
-            Some("off_info") => ev.off_info = String::from_utf8_lossy(user_attributes.get_value().unwrap_or_default()).to_string(),
-            Some("log") => ev.log = String::from_utf8_lossy(user_attributes.get_value().unwrap_or_default()).to_string(),
-            Some("stack") => ev.stack = String::from_utf8_lossy(user_attributes.get_value().unwrap_or_default()).to_string(),
+                let val = user_attributes.get_value().unwrap();
+                ev.time_type = read_u8_values(val);
+            }
+            Some("on_info") => ev.on_info = read_string_value(user_attributes.get_value()),
+            Some("off_info") => ev.off_info = read_string_value(user_attributes.get_value()),
+            Some("log") => ev.log = read_string_value(user_attributes.get_value()),
+            Some("stack") => ev.stack = read_string_value(user_attributes.get_value()),
             _ => (),
         }
     }
+
     if ev.start_time < 1600000000000000000 {
         return;
     }
+
     println!("{}", ev);
+
     let mut ca_guard = cca.lock().unwrap();
     ca_guard.put_event_to_segments(
         event.get_pid(),
@@ -92,23 +68,20 @@ pub fn consume_cpu_event(event: &KindlingEventForGo, cca: &Arc<Mutex<CpuAnalyzer
 }
 
 impl CpuAnalyzer {
+    pub fn new() -> Self {
+        CpuAnalyzer {
+            cpu_pid_events: HashMap::new(),
+            lock: Mutex::new(()),
+        }
+    }
+
     pub fn put_event_to_segments(&mut self, pid: u32, tid: u32, thread_name: String, event: CpuEvent) {
-        let _lock = self.lock.lock().unwrap(); // Acquire the lock
+        let _lock = self.lock.lock().unwrap();
 
         let tid_cpu_events = self.cpu_pid_events.entry(pid).or_insert_with(HashMap::new);
         let time_segments = tid_cpu_events.entry(tid).or_insert_with(|| {
             let base_time = event.start_timestamp() / NANO_TO_SECONDS;
-            let mut segments = CircleQueue::new(MAX_SEGMENT_SIZE);
-
-            for i in 0..MAX_SEGMENT_SIZE {
-                let segment = Segment::new(
-                    (base_time + (i as u64)) * NANO_TO_SECONDS,
-                    (base_time + (i as u64) + 1) * NANO_TO_SECONDS,
-                );
-                segments.update_by_index(i, segment);
-
-            }
-
+            let segments = create_initial_segments(base_time);
             TimeSegments {
                 pid,
                 tid,
@@ -119,54 +92,36 @@ impl CpuAnalyzer {
         });
 
         let end_offset = (event.end_timestamp() / NANO_TO_SECONDS - time_segments.base_time) as i32;
-
         if end_offset < 0 {
             return;
         }
 
         let start_offset = (event.start_timestamp() / NANO_TO_SECONDS - time_segments.base_time) as i32;
-
-        let clear_size = MAX_SEGMENT_SIZE / 2;
         let should_clear_segments = start_offset >= MAX_SEGMENT_SIZE as i32 || end_offset > MAX_SEGMENT_SIZE as i32;
 
         if should_clear_segments {
             if start_offset * 2 >= 3 * MAX_SEGMENT_SIZE as i32 {
-                // Clear all elements
-
                 time_segments.segments.clear();
                 time_segments.base_time = event.start_timestamp() / NANO_TO_SECONDS;
                 let end_offset = end_offset - start_offset;
                 let start_offset = 0;
-
-                for i in 0..MAX_SEGMENT_SIZE {
-                    let segment = Segment::new(
-                        (time_segments.base_time + (i as u64)) * NANO_TO_SECONDS,
-                        (time_segments.base_time + ((i + 1) as u64)) * NANO_TO_SECONDS,
-                    );
-                    time_segments.segments.update_by_index(i, segment);
-                }
+                time_segments.segments = create_initial_segments(time_segments.base_time);
             } else {
-                // Clear half of the elements
                 let clear_size = MAX_SEGMENT_SIZE / 2;
-
                 time_segments.base_time += clear_size as u64;
                 let mut start_offset = start_offset - clear_size as i32;
                 let end_offset = end_offset - clear_size as i32;
-
                 if start_offset < 0 {
                     start_offset = 0;
                 }
-
                 for i in 0..clear_size {
                     let moved_index = i + clear_size;
-                    let val = time_segments.segments.get_by_index(moved_index);
-                    if let Some(segment) = val {
+                    if let Some(segment) = time_segments.segments.get_by_index(moved_index) {
                         let mut cloned_segment = segment.clone();
                         cloned_segment.put_timed_event(event.clone());
                         cloned_segment.is_send = 0;
                         time_segments.segments.update_by_index(i, cloned_segment);
                     }
-
                     let segment_tmp = Segment::new(
                         (time_segments.base_time + (moved_index as u64)) * NANO_TO_SECONDS,
                         (time_segments.base_time + ((moved_index + 1) as u64)) * NANO_TO_SECONDS,
@@ -175,9 +130,6 @@ impl CpuAnalyzer {
                 }
             }
         }
-
-        // Update the thread name immediately
-        //time_segments.update_thread_name(thread_name);
 
         for i in start_offset..=end_offset.min(MAX_SEGMENT_SIZE as i32 - 1) {
             if let Some(segment) = time_segments.segments.get_by_index(i as usize) {
@@ -189,8 +141,37 @@ impl CpuAnalyzer {
         }
     }
 
-    pub fn print_cpu_pid_events(&mut self){
+    pub fn print_cpu_pid_events(&self) {
         println!("{:?}", self.cpu_pid_events);
     }
+}
 
+fn read_u64_values(val: &[u8]) -> Vec<u64> {
+    let mut cursor = Cursor::new(val);
+    let count = val.len() / 8;
+    let mut values = Vec::with_capacity(count);
+    for _ in 0..count {
+        values.push(cursor.read_u64::<LittleEndian>().unwrap());
+    }
+    values
+}
+
+fn read_u8_values(val: &[u8]) -> Vec<u8> {
+    val.to_vec()
+}
+
+fn read_string_value(val: Option<&[u8]>) -> String {
+    String::from_utf8_lossy(val.unwrap_or_default()).to_string()
+}
+
+fn create_initial_segments(base_time: u64) -> CircleQueue{
+    let mut segments = CircleQueue::new(MAX_SEGMENT_SIZE);
+    for i in 0..MAX_SEGMENT_SIZE {
+        let segment = Segment::new(
+            (base_time + (i as u64)) * NANO_TO_SECONDS,
+            (base_time + (i as u64) + 1) * NANO_TO_SECONDS,
+        );
+        segments.update_by_index(i, segment);
+    }
+    segments
 }
